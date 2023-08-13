34973ad69ada2a1a05232478f9458925fa7db5a9
Daniel <daniel@rossides.uk>
2023-08-13 13:18:16 +0100

    UI edit.

--- app/src/main/java/com/sky/recruitmenttest/views/components/MovieTile.kt
+++ app/src/main/java/com/sky/recruitmenttest/views/components/MovieTile.kt
@@ -3,15 +3,16 @@ package com.sky.recruitmenttest.views.components
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.sky.recruitmenttest.feature_moviesearch.domain.model.Movie
@@ -22,7 +23,7 @@ fun MovieTile(movie: Movie) {
         modifier = Modifier
             .size(height = 180.dp, width = 100.dp)
             .padding(all = 5.dp),
-        border = BorderStroke(2.dp, Color.Red)
+        border = BorderStroke(2.dp, Color.Black)
     ) {
         Box(modifier = Modifier.fillMaxSize()){
             AsyncImage(
@@ -30,10 +31,14 @@ fun MovieTile(movie: Movie) {
                 contentDescription = null,
                 modifier = Modifier.fillMaxSize()
             )
-            Column(modifier = Modifier.background(color = Color.White.copy(alpha = 0.7f))) {
-                Text(text = movie.title)
-                Text(text = movie.genre)
-            }
+            Text(
+                text = movie.title,
+                modifier = Modifier
+                    .background(color = Color.Black.copy(alpha = 0.7f))
+                    .align(alignment = Alignment.BottomCenter),
+                color = Color.White,
+                textAlign = TextAlign.Center
+            )
         }
     }
 }
